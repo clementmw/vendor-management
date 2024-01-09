76622d1c672b7518044808d80af8c276cd4a3d5b
Clement Macharia <clementmacharia62@gmail.com>
2024-01-10 00:19:32 +0300

    data

--- lib/click.py
+++ lib/click.py
@@ -1,9 +1,16 @@
 import click
+from faker import Faker
 from sqlalchemy.orm import sessionmaker
 from models import Base,engine,Vendor,Customer,Admin
 
+fake  = Faker()
+
 Session = sessionmaker(bind = engine)
 session = Session()
+
+
 session.commit()
+
+
 session.close()
 
