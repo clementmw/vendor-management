6a3312027df037edbce057a1aab4163830182779
Clement Macharia <clementmacharia62@gmail.com>
2024-01-10 15:34:22 +0300

    click

--- lib/cli.py
+++ lib/cli.py
@@ -1,13 +1,25 @@
 import click
+from seed import session
+
+@click.group #to handle the click groups
+def cli():
+    pass
+
+
 
 @click.command()
+def hello():
+    vendor= click.prompt('Enter vendor name')
+    click.echo(f'vendor {vendor} added sucessfully')
 
-@click.option('--name', '-n', default = 'mike', help = 'enter name')
-
-def hello(name):
-    click.echo(f'Hello {name}')
+@click.command()
+def product ():
+    product = click.prompt('Enter product name')
+    click.echo(f'product {product} added sucessfully')
 
 
 if __name__ == '__main__':
-    hello()
+    cli.add_command(hello)
+    cli.add_command(product)
+    cli()
 
