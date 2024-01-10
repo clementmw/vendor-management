07a6ca9133585cbb3ffd7fd366e16354436c3ddf
Clement Macharia <clementmacharia62@gmail.com>
2024-01-10 13:04:51 +0300

    #

--- lib/cli.py
+++ lib/cli.py
@@ -2,7 +2,7 @@ import click
 
 @click.command()
 
-@click.option('--name', '-n', default = 'mike' help = 'enter name')
+@click.option('--name', '-n', default = 'mike', help = 'enter name')
 
 def hello(name):
     click.echo(f'Hello {name}')
